f266efda582b86b1769a9b245d49af12cd3e29f5
NiKo <evil4niko@gmail.com>
2017-12-13 22:21:26 +0800

    rewrite

--- init.el
+++ init.el
@@ -3,60 +3,33 @@
 ;;; This file bootstraps the configuration, which is divided into
 ;;; a number of other files.
 
+(let ((minver "24.3"))
+  (when (version< emacs-version minver)
+    (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
+(when (version< emacs-version "24.5")
+  (message "Your Emacs is old, and some functionality in config will be disabled. Please upgrade if possible."))
 
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
+(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
+(defconst *is-a-mac* (eq system-type 'darwin))
 
-(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
-(require 'init-benchmarking) ;; Measure startup time
-
-;;---------------------------------------------------------------------------
+;;----------------------------------------------------------------------------
 ;; Adjust garbage collection thresholds during startup, and thereafter
-;;---------------------------------------------------------------------------
+;;----------------------------------------------------------------------------
 (let ((normal-gc-cons-threshold (* 20 1024 1024))
       (init-gc-cons-threshold (* 128 1024 1024)))
   (setq gc-cons-threshold init-gc-cons-threshold)
   (add-hook 'after-init-hook
-	    (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
 
-(require 'init-utils)
-(require 'init-elpa)
+;;----------------------------------------------------------------------------
+;; Bootstrap config
+;;----------------------------------------------------------------------------
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
-(require 'init-themes)
-
-(require 'init-org)
-
-(require 'init-company)
-(require 'init-helm)
-
-(require 'init-paredit)
-(require 'init-lisp)
-
-(require 'server)
-(unless (server-running-p)
-  (server-start))
 
 (provide 'init)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("9313e289c6d49c96e686ca377e35406dda0d851e26beb3493b427281dbd4fec8" default)))
- '(helm-gtags-auto-update t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
- ;; Local Variables:
+;; local Variables:
 ;; coding: utf-8
 ;; no-byte-compile: t
 ;; End:
--- lisp/init-paredit.el
+++
@@ -1,6 +0,0 @@
-(require-package 'paredit)
-(autoload 'enable-paredit-mode "paredit")
-
-(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
-
-(provide 'init-paredit)
