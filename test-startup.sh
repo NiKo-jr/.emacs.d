706a77e4e56d29471fa55253046dc4fde5876708
NiKo <evil4NiKo@gmail.com>
2018-02-19 21:42:53 +0800

    add test-startup

---
+++ test-startup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+if [ -n "$TRAVIS" ]; then
+    # Make it look like is ~/.emacs.d (needed for Emacs 24.3, at least)
+    export HOME=$PWD/..
+    ln -s emacs.d ../.emacs.d
+fi
+echo "Attempting startup..."
+${EMACS:=emacs} -nw --batch \
+               --eval '(let ((debug-on-error t)
+                             (url-show-status nil)
+                             (user-emacs-directory default-derectory)
+                             (user-init-file (expand-file-name "init.el"))
+                             (load-path (delq default-directory load-path)))
+                          (load-file user-init-file)
+                          (run-hooks (quote after-init-hook)))'
+echo "Startup successful"
